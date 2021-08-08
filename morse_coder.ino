252e6e519328f64b3fafd2bd0fe282570aa3e937
PmXa <cremosity@gmail.com>
2021-08-08 18:26:10 -0500

    Se usó un ciclo para imprimir por serial cada caracter de un mensaje

--- morse_coder.ino
+++ morse_coder.ino
@@ -4,17 +4,22 @@ This will be used when:
     - iterating over an array
     - translating it to proper morse code and
     - printing a message
-By PmXa (27-may-2020)
+By PmXa (31-may-2020)
 */
 
-int number = 10;
+char message[] = "Hola, mundo!";
+int numel;
 
 void setup(){
     Serial.begin(9600);
+    numel = sizeof(message);
+    numel = numel/sizeof(message[0]);
     
-    while(number > 0){
-        Serial.println("Hola, mundo!");
-        number = number - 1;
+    for (int x = 0; x < numel; x++){
+        Serial.println(message[x]);
+        // Waits for the data to be sent before sending more
+        // Not needed right now
+        // Serial.flush();
         delay(500);
     }
 }
